d8d9f9b3cfcb2abcdc777d4e4862184a8a61b6ed
elkuku <der.el.kuku@gmail.com>
2017-05-19 15:36:05 -0500

    Support for local repos in sha finder

--- src/AppBundle/Service/ShaFinder.php
+++ src/AppBundle/Service/ShaFinder.php
@@ -8,22 +8,37 @@
 
 namespace AppBundle\Service;
 
+/**
+ * Class ShaFinder
+ * @package AppBundle\Service
+ */
 class ShaFinder
 {
-	private $sha = 'n/a';
+    private $sha = 'n/a';
 
-	public function __construct($rootDir)
-	{
-		$dir = realpath($rootDir.'/..');
+    /**
+     * ShaFinder constructor.
+     *
+     * @param string $rootDir
+     */
+    public function __construct($rootDir)
+    {
+        $dir = realpath($rootDir.'/..');
 
-		if (file_exists($dir . '/sha.txt'))
-		{
-			$this->sha = file_get_contents($dir . '/sha.txt');
-		}
-	}
+        if (file_exists($dir.'/sha.txt')) {
+            $this->sha = file_get_contents($dir.'/sha.txt');
+        } elseif (file_exists($dir.'/.git/refs/heads/master')) {
+            $this->sha = file_get_contents($dir.'/.git/refs/heads/master');
+        }
+    }
 
-	public function getSha()
-	{
-		return $this->sha;
-	}
+    /**
+     * Get the current SHA.
+     *
+     * @return string
+     */
+    public function getSha()
+    {
+        return $this->sha;
+    }
 }
