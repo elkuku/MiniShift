adfce3ebc58deb5e2f8e9fa012be56e136a05a5d
elkuku <der.el.kuku@gmail.com>
2017-05-19 16:21:00 -0500

    More monkeys :)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,10 +6,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
@@ -26,5 +33,14 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'projects' => $projects,
         ]);
+    }
+
+    /**
+     * @Route("about", name="default.about")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function aboutAction()
+    {
+        return $this->render('default/about.html.twig');
     }
 }
