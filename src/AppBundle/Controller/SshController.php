3bc795b4098d41494f8aaac077a35806979af5ec
elkuku <der.el.kuku@gmail.com>
2017-05-22 09:13:20 -0500

    More on SSH

--- src/AppBundle/Controller/SshController.php
+++ src/AppBundle/Controller/SshController.php
@@ -55,4 +55,70 @@ class SshController extends Controller
 
         return $this->render('ssh/index.html.twig', ['keys' => $handler]);
     }
+
+    /**
+     * @Route("ssh-add-file", name="ssh.add-file")
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function addFromFileAction(Request $request): Response
+    {
+        $file = $request->files->get('key_file');
+        $handler = $this->get('app.ssh_handler');
+
+        if (!$file) {
+            $this->addFlash('danger', 'No file received!');
+        } else {
+            $path = $file->getRealPath();
+            if (!$path) {
+                $this->addFlash('danger', 'Invalid key file!');
+            } else {
+                try {
+                    $key = file_get_contents($path);
+                    $publicKey = new PublicKey($key);
+                    $handler->addKey($publicKey);
+                    $handler->save();
+                    $this->addFlash('success', 'The key hs been added');
+                } catch (\Exception $exception) {
+                    $this->addFlash('danger', $exception->getMessage());
+                }
+            }
+        }
+
+        return $this->render('ssh/index.html.twig', ['keys' => $handler]);
+    }
+
+    /**
+     * @Route("ssh-remove/{comment}", name="ssh.remove")
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     * @param string $comment
+     *
+     * @return Response
+     */
+    public function removeAction(string $comment)
+    {
+        $handler = $this->get('app.ssh_handler');
+
+        $found = false;
+
+        /* @type PublicKey $key */
+        foreach ($handler as $key) {
+            if ($key->getComment() == $comment) {
+                $handler->removeKey($key);
+                $handler->save();
+                $found = true;
+                $this->addFlash('success', 'The key hs been removed.');
+            }
+        }
+
+        if (!$found) {
+            $this->addFlash('danger', 'No such key!');
+        }
+
+        return $this->render('ssh/index.html.twig', ['keys' => $handler]);
+    }
 }
