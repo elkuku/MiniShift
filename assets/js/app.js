3f3ad29f29e85b7facc39d1507dd17c61e089d69
Nikolai Plath <der.el.kuku@gmail.com>
2018-05-27 17:02:33 -0500

    Fix JQuery not loaded

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,10 @@
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
 require('../../node_modules/bootstrap/dist/css/bootstrap.min.css')
 require('../../node_modules/bootstrap/dist/js/bootstrap.min')
+
+require('../css/app.css');
